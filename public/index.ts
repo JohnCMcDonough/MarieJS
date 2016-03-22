37daa62ce1426c18cb42860e9d068a57a743ae76
John McDonough <jmcdonough@vertical.com>
2016-03-22 15:39:00 -0400

    added lenting abilities. Requires changes to code erroring.

--- public/index.ts
+++ public/index.ts
@@ -12,13 +12,13 @@ a, DEC 10
 b, DEC 15`;
     codeError: string = "";
     lineError: number = -1;
-	objectError: string = "";
-	viewType: "HEX" | "ASCII" | "DEC" = "HEX";
+    objectError: string = "";
+    viewType: "HEX" | "ASCII" | "DEC" = "HEX";
 
     private debounceTimer = 0;
     public instructionsCount = 0;
 
-	private editor: CodeMirror.Editor;
+    private editor: CodeMirror.Editor;
 
     static $inject = ["$scope", "$rootScope"];
     constructor(private $scope: angular.IScope, private $rootScope: angular.IScope) {
@@ -39,96 +39,98 @@ b, DEC 15`;
     }
 
     updateCode() {
-		if (this.editor) {
-			// this.editor.clearGutter("note-gutter");
-			this.editor.getDoc().getAllMarks().forEach(mark => mark.clear())
-		}
-		this.codeError = "";
-		this.instructionsCount = 0;
-		if (this.interpreter && this.interpreter.pauseExecution) {
-			this.interpreter.pauseExecution()
-		}
-		try {
-			this.interpreter = new MarieInterpreter(this.code);
-			this.interpreter.onTick = () => {
-				this.instructionsCount++;
-				if (this.debounceTimer) {
-					clearTimeout(this.debounceTimer);
-				}
-				this.debounceTimer = setTimeout(this.safeApply.bind(this), 5);
-			}
-			this.interpreter.onOutput = () => {
-				// this.safeApply();
-			}
-			this.interpreter.onMemoryChangedDelegate = (mar, mbr) => {
-				this.$rootScope.$emit('memoryUpdate', mar, mbr);
-			}
-			this.interpreter.onExecutionFinished = () => {
-				console.info(this.interpreter.outputBuffer);
-			}
+        if (this.editor) {
+            // this.editor.clearGutter("note-gutter");
+            this.editor.getDoc().getAllMarks().forEach(mark => mark.clear())
+        }
+        this.codeError = "";
+        this.instructionsCount = 0;
+        if (this.interpreter && this.interpreter.pauseExecution) {
+            this.interpreter.pauseExecution()
+        }
+        try {
+            this.interpreter = new MarieInterpreter(this.code);
+            this.interpreter.onTick = () => {
+                this.instructionsCount++;
+                if (this.debounceTimer) {
+                    clearTimeout(this.debounceTimer);
+                }
+                this.debounceTimer = setTimeout(this.safeApply.bind(this), 5);
+            }
+            this.interpreter.onOutput = () => {
+                // this.safeApply();
+            }
+            this.interpreter.onMemoryChangedDelegate = (mar, mbr) => {
+                this.$rootScope.$emit('memoryUpdate', mar, mbr);
+            }
+            this.interpreter.onExecutionFinished = () => {
+                console.info(this.interpreter.outputBuffer);
+            }
 
-		}
-		catch (err) {
-			this.codeError = "Error on Line " + err.lineNumber + ": " + (<CompilerError>err).errorstring;
-			this.lineError = (<CompilerError>err).lineNumber - 1
-			this.objectError = (<CompilerError>err).object;
-			console.log("had error", this.editor, this.lineError, this.codeError);
-			if (this.editor) {
-				console.log("found editor object");
-				// var icon = document.createElement("p");
-				// icon.className = "fa fa-exclamation-circle note-gutter-text";
-				// var lm = this.editor.setGutterMarker(this.lineError, "note-gutter", icon);
-				// console.log(lm);
-				// this.editor.doc.addLineClass(this.lineError, "text", "line-error");
-				var line = this.editor.getDoc().getLine(this.lineError);
-				var char = line.indexOf(this.objectError);
-				if (char != -1)
-					this.editor.getDoc().markText({ line: this.lineError, ch: char }, { line: this.lineError, ch: char + this.objectError.length }, { className: "line-error" });
-				else {
-					this.editor.getDoc().markText({ line: this.lineError, ch: 0 }, { line: this.lineError, ch: line.length }, { className: "line-error" });
-				}
-				this.safeApply();
-			}
-		}
-	}
+        }
+        catch (err) {
+            (<Array<CompilerError>>err).forEach(err => {
+                this.codeError += "Error on Line " + err.lineNumber + ": " + (<CompilerError>err).errorstring;
+                this.lineError = (err).lineNumber - 1
+                this.objectError = (err).object;
+                console.log("had error", this.editor, this.lineError, "Error on Line " + err.lineNumber + ": " + (<CompilerError>err).errorstring);
+                if (this.editor) {
+                    console.log("found editor object");
+                    // var icon = document.createElement("p");
+                    // icon.className = "fa fa-exclamation-circle note-gutter-text";
+                    // var lm = this.editor.setGutterMarker(this.lineError, "note-gutter", icon);
+                    // console.log(lm);
+                    // this.editor.doc.addLineClass(this.lineError, "text", "line-error");
+                    var line = this.editor.getDoc().getLine(this.lineError);
+                    var char = line.indexOf(this.objectError);
+                    if (char != -1)
+                        this.editor.getDoc().markText({ line: this.lineError, ch: char }, { line: this.lineError, ch: char + this.objectError.length }, { className: "line-error" });
+                    else {
+                        this.editor.getDoc().markText({ line: this.lineError, ch: 0 }, { line: this.lineError, ch: line.length }, { className: "line-error" });
+                    }
+                    this.safeApply();
+                }
+            })
+        };
+    }
 
-	safeApply(fn?: () => void) {
-		var phase = this.$scope.$root.$$phase;
-		if (phase == '$apply' || phase == '$digest') {
-			if (fn && (typeof (fn) === 'function')) {
-				fn();
-			}
-		} else {
-			this.$scope.$apply(fn);
-		}
-	};
+    safeApply(fn?: () => void) {
+        var phase = this.$scope.$root.$$phase;
+        if (phase == '$apply' || phase == '$digest') {
+            if (fn && (typeof (fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$scope.$apply(fn);
+        }
+    };
 }
 app.controller("MainController", MainController);
 
 app.directive('ngAllowTab', function() {
-	return function(scope, element, attrs) {
-		element.bind('keydown', function(event) {
-			if (event.which == 9) {
-				event.preventDefault();
-				var start = this.selectionStart;
-				var end = this.selectionEnd;
-				element.val(element.val().substring(0, start)
-					+ '\t' + element.val().substring(end));
-				this.selectionStart = this.selectionEnd = start + 1;
-				element.triggerHandler('change');
-			}
-		});
-	};
+    return function(scope, element, attrs) {
+        element.bind('keydown', function(event) {
+            if (event.which == 9) {
+                event.preventDefault();
+                var start = this.selectionStart;
+                var end = this.selectionEnd;
+                element.val(element.val().substring(0, start)
+                    + '\t' + element.val().substring(end));
+                this.selectionStart = this.selectionEnd = start + 1;
+                element.triggerHandler('change');
+            }
+        });
+    };
 });
 
 app.directive('memoryTable', () => {
-	return {
-		restrict: 'A',
-		scope: {
-			memory: '=',
-			viewtype: "=",
-		},
-		template: `
+    return {
+        restrict: 'A',
+        scope: {
+            memory: '=',
+            viewtype: "=",
+        },
+        template: `
 		<div class="mariejs-memoryTable">
 			<table class="header">
 				<thead>
@@ -153,34 +155,34 @@ app.directive('memoryTable', () => {
 				</table>
 			</div>
 		</div>`,
-		controller: ["$scope", "$rootScope", ($scope: angular.IScope, $rootScope: angular.IScope) => {
-			$scope['onChange'] = {};
-			function fillMemory() {
-				if (!$scope['memory']) {
-					$scope['memory'] = new Int16Array(2048);
-				}
+        controller: ["$scope", "$rootScope", ($scope: angular.IScope, $rootScope: angular.IScope) => {
+            $scope['onChange'] = {};
+            function fillMemory() {
+                if (!$scope['memory']) {
+                    $scope['memory'] = new Int16Array(2048);
+                }
 
-				$scope['cols'] = [];
-				for (var i = 0; i < 16; i++) {
-					$scope['cols'].push(i);
-				}
+                $scope['cols'] = [];
+                for (var i = 0; i < 16; i++) {
+                    $scope['cols'].push(i);
+                }
 
-				$scope['rows'] = [];
-				for (var i = 0; i < $scope['memory'].length; i += 16) {
-					$scope['rows'].push(i);
-				}
-			}
-			fillMemory();
+                $scope['rows'] = [];
+                for (var i = 0; i < $scope['memory'].length; i += 16) {
+                    $scope['rows'].push(i);
+                }
+            }
+            fillMemory();
 
-			$rootScope.$on('memoryUpdate', (e, address, newValue) => {
-				$scope['onChange'][address] = true;
-				setTimeout(() => {
-					$scope['onChange'][address] = false;
-					$scope.$apply();
-				}, 50);
-			})
-		}]
-	};
+            $rootScope.$on('memoryUpdate', (e, address, newValue) => {
+                $scope['onChange'][address] = true;
+                setTimeout(() => {
+                    $scope['onChange'][address] = false;
+                    $scope.$apply();
+                }, 50);
+            })
+        }]
+    };
 });
 
 app.filter('toASCII', () => (x) => {
